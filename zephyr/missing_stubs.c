036dfd60d36bffdaf8d4f8de9cfef07688387f02
tenllado@ucm.es <tenllado@ucm.es>
2023-05-25 13:03:05 +0200

    missing stubs weak symbols

--- zephyr/missing_stubs.c
+++ zephyr/missing_stubs.c
@@ -3,13 +3,15 @@
 #include <errno.h>
 #include <sys/times.h>
 
-/* FIXME: this stubs should be properly implemented, in zephyr itself or here.
- * This is just a temporal non-working implementation to start testing lua with
- * zephyr.
+/* This stubs should be properly implemented, in zephyr itself or in your
+ * project. We provide just a temporal non-working implementation to start
+ * testing lua with zephyr, but they will be removed if they become available
+ * in the libc libraries shipped with zephyr.
  */
 
 #ifdef CONFIG_PICOLIBC
 
+clock_t times(struct tms* tms) __attribute__((weak));
 clock_t times(struct tms* tms)
 {
 	errno = ENOTSUP;
@@ -20,12 +22,14 @@ clock_t times(struct tms* tms)
 
 #ifdef CONFIG_NEWLIB_LIBC
 
+int _unlink(const char *pathname) __attribute__((weak));
 int _unlink(const char *pathname)
 {
 	errno = ENOTSUP;
 	return -1;
 }
 
+int _open(const char *pathname) __attribute__((weak));
 int _open(const char *pathname)
 {
 	errno = ENOTSUP;
@@ -33,6 +37,7 @@ int _open(const char *pathname)
 }
 
 
+int _times(const char *pathname) __attribute__((weak));
 int _times(const char *pathname)
 {
 	errno = ENOTSUP;
