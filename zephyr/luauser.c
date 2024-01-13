5d0f8c1cff536bcaa0db3ee8f00c54bff16b25ed
Christian Tenllado <tenllado@ucm.es>
2024-01-13 10:26:16 +0100

    native zephyr api for global interpreter lock

--- zephyr/luauser.c
+++ zephyr/luauser.c
@@ -4,45 +4,6 @@
 
 #define _UNUSED(...) (void)(__VA_ARGS__)
 
-#ifdef CONFIG_POSIX_API
-
-static struct {
-	pthread_mutex_t m;
-	int init;
-} glock;
-
-void luauser_stateopen(lua_State *L)
-{
-	_UNUSED(L);
-	if (!glock.init) {
-		pthread_mutex_init(&glock.m, NULL);
-		glock.init = 1;
-	}
-}
-
-void luauser_stateclose(lua_State *L)
-{
-	_UNUSED(L);
-	if (glock.init) {
-		pthread_mutex_destroy(&glock.m);
-		glock.init = 0;
-	}
-}
-
-void luauser_lock(lua_State *L)
-{
-	_UNUSED(L);
-	pthread_mutex_lock(&glock.m);
-}
-
-void luauser_unlock(lua_State *L)
-{
-	_UNUSED(L);
-	pthread_mutex_unlock(&glock.m);
-}
-
-#else
-
 static struct {
 	struct k_mutex m;
 	int init;
@@ -76,5 +37,3 @@ void luauser_unlock(lua_State *L)
 	_UNUSED(L);
 	k_mutex_unlock(&glock.m);
 }
-#endif
-
